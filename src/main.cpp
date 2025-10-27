7fb414647a56f5acd9d41c9e3e7bca92ccb71540
kolpakov <kolpakov@168robotics.com>
2025-10-27 19:26:45 +0300

    Added SFML library as a submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+#include <optional>
 
 int main()
 {
-    std::cout << "Hello NG\n";
-    return 0;
+    sf::Window window(sf::VideoMode({800, 600}), "Hello NG");
+
+    while (window.isOpen())
+    {
+        while (std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
 }
